3b54710a61ba8b4c259b5535f43677721b1327d8
zigzacvy <lethanhphuong17051989@gmail.com>
2022-07-08 06:42:24 +0700

    Refactor Input and select

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <label>{{label}}</label>
+  <input
+    :placeholder="label"
+    :value="modelValue"
+    v-bind="{
+      ...$attrs,
+      onInput: ($event) => {$emit('update:modelValue', $event.target.value)}
+    }"
+    class="field"
+  >
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
